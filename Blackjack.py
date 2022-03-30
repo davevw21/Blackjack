d7c8d6f27430a7e5b8dc382b22b31d3c68d70301
davevw21 <vanwdw@students.fscj.edu>
2022-03-30 10:02:18 -0700

    Fixed the dealing of cards to show only cards dealt

--- Blackjack.py
+++ Blackjack.py
@@ -2,10 +2,16 @@ import requests
 import json
 import pprint
 
-#name = input("Please enter your name:")
-#dc = input("How many decks would you like?")
-name = "David"
-dc = "6"
+#ask for inputs such as name and deck count.
+
+name = input("Please enter your name:")
+dc = input("How many decks would you like?")
+
+print("Welcome " + name + " lets play blackjack, we will be playing with " + dc + " decks.")
+
+
+#shuffle the deck
+
 url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + str(dc)
 
 payload={}
@@ -22,7 +28,7 @@ id = deckJson['deck_id']
 print("The cards have been shuffled for you " + name + " your deck id is" + id )
 #print(response.text)
 
-url = "https://deckofcardsapi.com/api/deck/16ga7moqqbll/draw/?count=1"
+url = "https://deckofcardsapi.com/api/deck/16ga7moqqbll/draw/?count=2"
 
 payload={}
 headers = {}
@@ -31,9 +37,12 @@ drawresponse = requests.request("GET", url, headers=headers, data=payload)
 
 handJson = drawresponse.json()
 cards = handJson['cards']
-for items in cards[0:3]:
-    print(code)
-print(cards[0:3])
+
+for c in range(len(cards)):
+    print(cards[c]["value"])
+#for items in cards[0:3]:
+    #print(code)
+#print(cards[0:3])
 #for cards in handJson():
     #print(cards['code'])
 #print(handJson['cards']['value'])
